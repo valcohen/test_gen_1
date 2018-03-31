0c4f269970686eb4422cbc0686b10c013df429c3
Val Cohen <val@valcohen.com>
2018-03-31 11:24:50 -0700

    Add sphere scene

---
+++ Assets/Scripts/MakeSphere.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeSphere : MonoBehaviour {
+    public Transform    baseObject;
+    public int          steps;
+    public int          timeScale = 1;
+
+    public Vector3 xScale = new Vector3 (1.00f, 0.01f, 0.01f);
+    public Vector3 yScale = new Vector3 (0.01f, 1.00f, 0.01f);
+    public Vector3 zScale = new Vector3 (0.01f, 0.01f, 1.00f);
+
+
+
+    private static Vector3 xAxis = new Vector3(1, 0, 0);
+    private static Vector3 yAxis = new Vector3(0, 1, 0);
+    private static Vector3 zAxis = new Vector3(0, 0, 1);
+
+    // Use this for initialization
+	void Start () {
+        Vector3     initialPos = new Vector3 (0, 1, 0);
+        float       interval = 180f / steps;    // 180 = half-circle to avoid drawing on top of ourselves
+
+        genCubesOnAxis (initialPos, interval, xAxis, zScale);
+        genCubesOnAxis (initialPos, interval, yAxis, xScale);
+        genCubesOnAxis (initialPos, interval, zAxis, yScale);
+		
+	}
+
+    void genCubesOnAxis (Vector3 initialPos, float interval, Vector3 axis, Vector3 scale)
+    {
+        Quaternion  rotation;
+        GameObject  newObject;
+
+        for (int i = 0; i < steps; i++) {
+            float angle = i * interval;
+            rotation = Quaternion.AngleAxis (angle, axis);
+
+            newObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            newObject.transform.position        = initialPos;
+            newObject.transform.localScale      = scale;
+            newObject.transform.localRotation   = rotation;
+
+            newObject.transform.SetParent (baseObject);
+            // newObject = Instantiate (baseObject, initialPos, rotation);
+
+            // setColor
+            float h, s, v;
+            h = angle / 180f;
+            s = 1f;
+            v = 1f;
+            Color newColor = Color.HSVToRGB (h, s, v);
+            Material material = newObject.GetComponent<Renderer> ().material;
+            material.color = newColor;
+            UnityEngine.Debug.Log (string.Format ("interval: {0}, rot: {1}, hue: {2}", interval, angle, h));
+            UnityEngine.Debug.Log (string.Format ("cube: {0}", newObject.transform.localRotation));
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float interval = timeScale * Time.deltaTime;
+
+        // Rotate the object around the World's X axis at 1 degree per second
+        baseObject.transform.Rotate(Vector3.right * interval, Space.World);
+
+        // ...also rotate around the World's Y axis
+        baseObject.transform.Rotate(Vector3.up * interval, Space.World);
+
+        // .. and Z
+        baseObject.transform.Rotate(Vector3.forward * interval, Space.World);
+
+
+
+	}
+}
