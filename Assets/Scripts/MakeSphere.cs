441f07d52e2ec67e74d08e23a1fadf47d6de91ca
Val Cohen <val@valcohen.com>
2018-03-31 22:55:20 -0700

    Switch to cylinders
    Switch from scaling cubes to rotating cylinders.

--- Assets/Scripts/MakeSphere.cs
+++ Assets/Scripts/MakeSphere.cs
@@ -7,6 +7,9 @@ public class MakeSphere : MonoBehaviour {
     public int          steps;
     public int          timeScale = 1;
 
+    public Vector3 objectScale = new Vector3 (0.01f, 1.00f, 0.01f);
+
+    // use these for cubes: instead of rotating, stretch along one axis
     public Vector3 xScale = new Vector3 (1.00f, 0.01f, 0.01f);
     public Vector3 yScale = new Vector3 (0.01f, 1.00f, 0.01f);
     public Vector3 zScale = new Vector3 (0.01f, 0.01f, 1.00f);
@@ -14,9 +17,11 @@ public class MakeSphere : MonoBehaviour {
     public float sphereScaleIncrement = 0.01f;
 
     // private members
-    private static Vector3 xAxis = new Vector3(1, 0, 0);
-    private static Vector3 yAxis = new Vector3(0, 1, 0);
-    private static Vector3 zAxis = new Vector3(0, 0, 1);
+    private static Vector3 xAxis = new Vector3(90, 0, 0);
+    private static Vector3 yAxis = new Vector3(0, 120, 0);
+    private static Vector3 zAxis = new Vector3(0, 0, 120);
+
+    private static Vector3 Ninety = new Vector3(90, 90, 90);
 
     private GameObject sphere;
     private Vector3 sphereStartScale = new Vector3 (0.01f, 0.01f, 0.01f);
@@ -27,30 +32,41 @@ public class MakeSphere : MonoBehaviour {
         Vector3     initialPos = new Vector3 (0, 0, 0);
         float       interval = 180f / steps;    // 180 = half-circle to avoid drawing on top of ourselves
 
-        genCubesOnAxis (initialPos, interval, xAxis, zScale);
-        genCubesOnAxis (initialPos, interval, yAxis, xScale);
-        genCubesOnAxis (initialPos, interval, zAxis, yScale);
+        var disc = genCubesOnAxis (initialPos, interval, xAxis, objectScale);
+        // genCubesOnAxis (initialPos, interval, yAxis, objectScale);
+        // genCubesOnAxis (initialPos, interval, zAxis, objectScale);
 	
-        genSphere (initialPos);
+        for (int i = 0; i < steps; i++) {
+            float angle = i * interval;
+            Instantiate (disc, baseObject.position, Quaternion.AngleAxis (angle, baseObject.up), baseObject);
+        }
+
+
+        // genSphere (initialPos);
 
 
 	}
 
-    void genCubesOnAxis (Vector3 initialPos, float interval, Vector3 axis, Vector3 scale)
+    GameObject genCubesOnAxis (Vector3 initialPos, float interval, Vector3 axis, Vector3 scale)
     {
+        GameObject baseObj = new GameObject ("base");
+        baseObj.transform.position = initialPos;
+        baseObj.transform.SetParent (baseObject.transform, false);
+
+
         Quaternion  rotation;
         GameObject  newObject;
 
         for (int i = 0; i < steps; i++) {
             float angle = i * interval;
-            rotation = Quaternion.AngleAxis (angle, axis);
+            rotation = Quaternion.AngleAxis (angle, axis) ;
 
-            newObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            newObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
             newObject.transform.position        = initialPos;
             newObject.transform.localScale      = scale;
             newObject.transform.localRotation   = rotation;
 
-            newObject.transform.SetParent (baseObject, false);  // worldPosStayes false = keep local pos
+            newObject.transform.SetParent (baseObj.transform, false);  // worldPositionStays false = keep local pos
             // newObject = Instantiate (baseObject, initialPos, rotation);
 
             // setColor
@@ -63,7 +79,9 @@ public class MakeSphere : MonoBehaviour {
             material.color = newColor;
             UnityEngine.Debug.Log (string.Format ("interval: {0}, rot: {1}, hue: {2}", interval, angle, h));
             UnityEngine.Debug.Log (string.Format ("cube: {0}", newObject.transform.localRotation));
+
         }
+        return baseObj;
     }
 
     void genSphere (Vector3 initialPos)
@@ -92,12 +110,13 @@ public class MakeSphere : MonoBehaviour {
         baseObject.transform.Rotate(Vector3.up      * interval);    // Y 
         baseObject.transform.Rotate(Vector3.forward * interval);    // Z
 
-        // setColor
+        // set sphere Color
+        /*
         Material material = sphere.GetComponentInParent<Renderer>().material;
         float h, s, v;
 
         Color.RGBToHSV (material.color, out h, out s, out v);
-        h += interval / 100; 
+        h += interval / 10; 
         s = 1f;
         v = 1f;
         material.color = Color.HSVToRGB (h, s, v);
@@ -118,8 +137,8 @@ public class MakeSphere : MonoBehaviour {
                                     : sphereStartScale;
 
 
-        UnityEngine.Debug.Log (string.Format ("interval: {0}, hue: {0}", interval, h));
-
+        UnityEngine.Debug.Log (string.Format ("interval: {0}, hue: {0}", interval, 
+        */
 
 
 	}
